025b635340d233883276468adefd18e934a45ab7
Christophe Fergeau <cfergeau@redhat.com>
2020-05-17 20:14:21 +0200

    Fix 'localhst' typo

--- build/bcc-build-image.sh
+++ build/bcc-build-image.sh
@@ -1,3 +1,3 @@
 KERNEL_VERSION=4.18.0-147.8.1.el8_1
 podman build --build-arg=KERNEL_VERSION=$KERNEL_VERSION -t bcc-tcpconnect . .
-podman push localhst/bcc-tcpconnect quay.io/cfergeau/bcc-tcpconnect:$KERNEL_VERSION
+podman push localhost/bcc-tcpconnect quay.io/cfergeau/bcc-tcpconnect:$KERNEL_VERSION
