41d3a298d3ba239f2bf1de474e19e07b221fcf71
Christophe Fergeau <cfergeau@redhat.com>
2020-08-05 11:14:32 +0200

    Use OpenShift 4.5.1 kernel

--- build/bcc-build-image.sh
+++ build/bcc-build-image.sh
@@ -1,3 +1,3 @@
-KERNEL_VERSION=4.18.0-147.8.1.el8_1
+KERNEL_VERSION=4.18.0-193.12.1.el8_2
 podman build --build-arg=KERNEL_VERSION=$KERNEL_VERSION -t bcc-tcpconnect . .
 podman push localhost/bcc-tcpconnect quay.io/teuf/bcc-tcpconnect:$KERNEL_VERSION
